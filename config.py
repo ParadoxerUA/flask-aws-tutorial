7065cbf63dec39a1e0ba8b47756062b967e68bbe
Bohdan Lomako <lomakojob@gmail.com>
2020-08-08 15:51:05 +0300

    refactored tutor code to be cleaner

--- config.py
+++ config.py
@@ -1,13 +1,6 @@
-# edit the URI below to add your RDS password and your AWS URL
-# The other elements are the same as used in the tutorial
-# format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
+class ProductionConfig:
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:adminadmin@flasktest.cp2vtctxjxkv.eu-central-1.rds.amazonaws.com:3306/flasktest'
-
-# Uncomment the line below if you want to work with a local DB
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
-
-SQLALCHEMY_POOL_RECYCLE = 3600
-
-WTF_CSRF_ENABLED = True
-SECRET_KEY = '87af05b67bf4aca75e2119175d1fa18c'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:adminadmin@flasktest.cp2vtctxjxkv.eu-central-1.rds.amazonaws.com:3306/flasktest'
+    SQLALCHEMY_POOL_RECYCLE = 3600
+    WTF_CSRF_ENABLED = True
+    SECRET_KEY = '87af05b67bf4aca75e2119175d1fa18c'
